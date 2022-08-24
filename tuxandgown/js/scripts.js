6681c54c7c9cdda3797c8bca1a15bea7e2162753
Brandon Swan <64652170+arcaneblunder@users.noreply.github.com>
2022-08-24 13:09:04 -0600

    Create scripts.js

---
+++ tuxandgown/js/scripts.js
@@ -0,0 +1,11 @@
+/*
+    Student Name: Brandon Swan
+    File Name: script.js
+    Date: April 25th, 2022
+*/
+
+//jQuery for hero image to take up header space
+
+$(document).ready(function() {
+	$('.hero').height($(window).height());
+});
