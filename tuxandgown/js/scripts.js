097c8c870e63dc190941527c1f01b885fac61e46
Brandon Swan <64652170+arcaneblunder@users.noreply.github.com>
2022-08-26 09:04:52 -0600

    Added alert

--- tuxandgown/js/scripts.js
+++ tuxandgown/js/scripts.js
@@ -1,18 +1,26 @@
-"use strict";
+/************************************************************************
+*Original Author: Brandon Swan
+*Date Created: 08/26/2022
+*Version: 0.1
+*Date Last Modified: 08/26/2022
+*Modified by: Brandon Swan
+*Modification log: 
 
-window.alert("This is a test of the alert method");
-window.alert("This is a test of the alert method");
-const joinList = function {
+    Version 0.0 - 08/26/2022 - Added prompt to website.
+
+    
+***
+******************************************************************** */
+
+"use strict";
+function incompleteAlert() {
+    window.alert("This website is a work in progress and some items will not function.");
+}
+
+/* Future Form validation
+const joinMailList = function {
     // objectName.methodName(parameters)
     // window.alert("This is a test of the alert method");
     // 
 }
-
-/*
-    Student Name: Brandon Swan
-    File Name: script.js
-    Date: ////
 */
-
-//javascript following along in class for practice.
-
